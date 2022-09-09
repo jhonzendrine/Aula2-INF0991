043895f0f164762c6133574e779ae6d858263969
Jhonatan Zendrine <jhonatan.zendrine@gmail.com>
2022-09-09 14:07:29 -0300

    Versão 3.0 Branch Fix35

--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@ namespace INF099
 {
     public class OlaMundo
     {   
-        Versão 1.0
-        Versão 2.0
+        //Versão 1.0
+        //Versão 2.0
+        
         static void Main(string[] args) {
             Console.WriteLine("Olá Professor!");
+
+        //Versão 3.0
         }
     }
 }
